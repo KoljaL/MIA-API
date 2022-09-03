cfc455fcfe0e8d3bcd859f9c8bced684af03b463
Kolja <lasar@rasal.de>
2022-09-03 11:17:24 +0200

    ADD docs

---
+++ docs/data.js
@@ -0,0 +1,14 @@
+const data = {
+    baseURL: "./files/",
+    ghLink: "https://github.com/KoljaL/",
+    headerTitle: "JOBSTORIES - Doku",
+    files: [{
+        title: "home",
+        url: "readme.md",
+        class: "home"
+    }, {
+        title: "API-Docs",
+        url: "API-Docs.md",
+        class: "api"
+    }]
+};
